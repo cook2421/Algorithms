88d148dfb8e31d220610365509ed91ac9d2384f9
anylogic_국승현 <cooksh2421>
2020-04-20 18:00:58 +0900

    가장 큰 수 푸는 중

--- src/Sort/K번째수.java
+++ src/Sort/K번째수.java
@@ -1,25 +1,27 @@
 package Sort;
 // https://programmers.co.kr/learn/courses/30/lessons/42748?language=java
 
+import java.util.Arrays;
+
 public class K번째수 {
     public static void main(String[] args) {
         int[] array = {1,5,2,6,3,7,4};
         int[][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
 
         int[] result = solution(array, commands);
-        if (result == new int[]{5,6,3}) {
-            System.out.println("성공!!");
-        } else {
-            System.out.println("실패!!");
-        }
+        System.out.println(Arrays.toString(result));
     }
 
 
     public static int[] solution(int[] array, int[][] commands){
-        int[] answer = {};
+        int[] answer = new int[commands.length];
 
-
-
+        for(int i=0; i<commands.length; i++){
+            int[] command = commands[i];
+            int[] temp = Arrays.copyOfRange(array, command[0]-1, command[1]);
+            Arrays.sort(temp);
+            answer[i] = temp[command[2]-1];
+        }
 
         return answer;
     }
---
+++ src/Sort/가장_큰_수.java
@@ -0,0 +1,20 @@
+package Sort;
+// https://programmers.co.kr/learn/courses/30/lessons/42746?language=java
+
+public class 가장_큰_수 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 30, 34, 5, 9};
+        int[] numbers2 = {6, 10, 2};
+
+        String result = solution(numbers);
+
+        System.out.println(result);
+    }
+
+
+    public static String solution(int[] numbers){
+        String answer = "";
+
+        return answer;
+    }
+}
