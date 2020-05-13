1f89fba42a253381fb9d12c0c5ebf214cdbcfc55
anylogic_국승현 <cooksh2421>
2020-05-13 17:27:35 +0900

    pull

--- src/BruteForce/소수_찾기.java
+++ src/BruteForce/소수_찾기.java
@@ -1,6 +1,9 @@
 package BruteForce;
 // https://programmers.co.kr/learn/courses/30/lessons/42839?language=java
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 소수_찾기 {
     public static void main(String[] args) {
 
@@ -13,8 +16,18 @@ public class 소수_찾기 {
     public static int solution(String numbers){
         int answer = 0;
 
-        String[] integerStrings = numbers.split("");
-        int[] integers = new int[numbers.length()];
+        String[] integers = numbers.split("");
+        Set<Integer> candidates = new HashSet<>();
+        Set<Integer> primeNumbers = new HashSet<>();
+
+        for(int i=0; i<integers.length; i++){
+            if(Integer.parseInt(integers[i]) == 2){
+                candidates.add(Integer.parseInt(integers[i]));
+            }
+            if(Integer.parseInt(integers[i])/2 != 0 && Integer.parseInt(integers[i]) != 5){
+
+            }
+        }
 
         return answer;
     }
