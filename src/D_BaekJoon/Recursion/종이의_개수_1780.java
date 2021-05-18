e4e42f2741f88832bc6d0ee40763562c3f27bb2a
anylogic_국승현 <cooksh2421>
2021-05-18 12:09:44 +0900

    종이의 개수 푸는 중

--- src/D_BaekJoon/Recursion/종이의_개수_1780.java
+++ src/D_BaekJoon/Recursion/종이의_개수_1780.java
@@ -3,6 +3,7 @@ package D_BaekJoon.Recursion;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 종이의_개수_1780 {
@@ -26,8 +27,53 @@ public class 종이의_개수_1780 {
             }
         }
 
-
+        countPaper(paper);
+        System.out.println(negative);
+        System.out.println(zero);
+        System.out.println(positive);
 
     }
 
+    public static void countPaper(int[][] paper){
+        int cntOrSplit = needToSplit(paper, paper.length);
+        switch (cntOrSplit){
+            case -1 : negative++; break;
+            case 0 : zero++; break;
+            case 1 : positive++; break;
+            case 2 :
+                countPaper(split(paper, 0, paper.length/3, 0, paper.length/3));
+                countPaper(split(paper, paper.length/3, paper.length/3*2, 0, paper.length/3));
+                countPaper(split(paper, paper.length/3*2, paper.length, 0, paper.length/3));
+
+                countPaper(split(paper, 0, paper.length/3, paper.length/3, paper.length/3*2));
+                countPaper(split(paper, paper.length/3, paper.length/3*2, paper.length/3, paper.length/3*2));
+                countPaper(split(paper, paper.length/3*2, paper.length, paper.length/3, paper.length/3*2));
+
+                countPaper(split(paper, 0, paper.length/3, paper.length/3*2, paper.length));
+                countPaper(split(paper, paper.length/3, paper.length/3*2, paper.length/3*2, paper.length));
+                countPaper(split(paper, paper.length/3*2, paper.length, paper.length/3*2, paper.length));
+                break;
+        }
+
+    }
+
+    public static int needToSplit(int[][] paper, int size){
+        int standard = paper[0][0];
+
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                if(paper[i][j] != standard) return 2;
+            }
+        }
+
+        return standard;
+    }
+
+    public static int[][] split(int[][] paper, int from_h, int to_h, int from_v, int to_v){
+        int[][] result = Arrays.copyOfRange(paper, from_v, to_v);
+        for(int i=0; i<result.length; i++){
+            result[i] = Arrays.copyOfRange(result[i], from_h, to_h);
+        }
+        return result;
+    }
 }
